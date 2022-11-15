f87d0b1490d59a9823bf5a6763e36bd2da3b4c2b
Luiz Gustavo Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-11-15 02:15:30 -0300

    feat: adiciona o create_collection dos users

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from src.app import app, mongo_client
 from src.app.routes import routes
 from src.app.models.roles import create_collection_roles
+from src.app.models.users import create_collection_users
 from flask.cli import with_appcontext
 import click
 
@@ -10,6 +11,7 @@ routes(app)
 @with_appcontext 
 def call_command():
   create_collection_roles(mongo_client)
+  create_collection_users(mongo_client)
 
 
 app.cli.add_command(call_command)
