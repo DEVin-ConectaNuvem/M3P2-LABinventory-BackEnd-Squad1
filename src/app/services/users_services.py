016e83dfb0cd82417ec83bcababe4c87e3cc1eb6
Luiz Gustavo Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-11-16 03:29:17 -0300

    feat: adicionado endpoint de login

--- src/app/services/users_services.py
+++ src/app/services/users_services.py
@@ -1,15 +1,15 @@
-import bcrypt 
+from datetime import datetime, timedelta
 from src.app import mongo_client
-from src.app.utils import exist_key
+from bson import json_util
+from src.app.utils import generate_jwt, exist_key
+import bcrypt
+
 
 class User():  
   def __init__(self, email, password, role):
     self.email = email
     self.password = password
     self.role = role
-
-  def check_password(self, password):
-    return bcrypt.checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
 
   @classmethod
   def seed(cls, email, password, role):
@@ -36,6 +36,36 @@ class User():
       }
     )
 
+
+def check_password(self, password):
+  return bcrypt.checkpw(password.encode("utf-8"), self['password'].encode("utf-8"))
+
+
+def login_user(request_data):
+  try:
+
+    list_keys = ["email", "password"]
+    data = exist_key(request_data, list_keys)
+
+    user_query = mongo_client.users.find_one({'email': data['email']})
+
+    if user_query == None:
+      return { "error": "Suas credênciais estão incorretas!", "status_code": 401 }
+    elif not check_password(user_query, data['password']):
+      return { "error": "Suas credênciais estão incorretas!", "status_code": 401 }
+    
+    payload = {
+      "user_id": str(user_query['_id']),
+      "exp": datetime.utcnow() + timedelta(days=1),
+      "roles": str(user_query['role'])
+    }
+
+    token = generate_jwt(payload)
+
+    return { "token": token }
+
+  except:
+    return { "error": "Algo deu errado!", "status_code": 500 }
 
 
 def create_user(request_data):
