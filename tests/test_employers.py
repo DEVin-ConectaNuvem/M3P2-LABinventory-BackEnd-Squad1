95efb424052edf2144d03d8b5c42622a6a75e012
rafatellescarneiro <rafatellescarneiro@gmail.com>
2022-11-19 19:56:06 -0300

    feat: definido teste para formato de cpf invalido cadastro de employers

--- tests/test_employers.py
+++ tests/test_employers.py
@@ -22,4 +22,27 @@ def test_create_employers_missing_fields(employee):
     assert response.staus_code == 400
     assert response.json['error'] == "Está faltando algum item requerido"
     
+def test_create_employer_invalid_cpf_format(employee):
+    data = {
+        "_id": fake._id(), 
+        "name": fake.name(), 
+        "birthDay": fake.birthDay(), 
+        "email": fake.email(), 
+        "phone": fake.phone(), 
+        "gender": fake.gender(), 
+        "zipcode": 1235, 
+        "city": fake.city(), 
+        "state": fake.state(), 
+        "street": fake.street(), 
+        "houseNumber": fake.houseNumber(), 
+        "neighborhood": fake.neighborhood(), 
+        "position": fake.position(), 
+        "complement": fake.complement(), 
+        "reference": fake.reference()
+    }
+    
+    response = employee.post("employers/create", data=json.dumps(data), headers=headers)
 
+    assert response.status_code == 400
+    assert response.json['error'] == "Formato de CEP inválido!"
+    
