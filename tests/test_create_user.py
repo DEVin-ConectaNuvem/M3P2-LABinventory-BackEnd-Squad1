4c6ef1a8e0dd06e8ffc04da47add0106868f4f4f
Thiago William <thiago.santos@conectanuvem.com.br>
2022-11-18 21:45:24 -0300

    feat(c-18) testa se é possível criar um usuário com email existente

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -35,3 +35,16 @@ def test_create_user_missing_fields(client):
 
 		assert response.status_code == 400
 		assert response.json['error'] == "Está faltando o item ['password']"
+  
+  
+def test_create_user_with_existing_email(client):
+		data = {
+			"email": fake.email(),
+			"password": "12345678"
+		}
+
+		client.post("users/create", data=json.dumps(data), headers=headers)
+		response = client.post("users/create", data=json.dumps(data), headers=headers)
+
+		assert response.status_code == 400
+		assert response.json['error'] == "Usuário já existente!"
