3ad122c804f6cc45e6fbf6939d3257299a4ebe43
Eduardo Martins Ribeiro <edumartinsrib@live.com>
2022-11-25 23:39:10 -0300

    fix: ajustes testes incorretos

---
+++ tests/test_employees.py
@@ -0,0 +1,47 @@
+import json
+
+mimetype = "application/json"
+url = "/employers/create"
+
+headers = {"Content-Type": mimetype, "Accept": mimetype}
+
+
+data = {
+    "name": "João da Silva",
+    "email": "joaosilvaTeste@gmail.com",
+    "phone": "(11) 9999-9999",
+    "position": "Desenvolvedor Backend",
+    "gender": "Masculino",
+    "zipcode": "855010070",
+    "birthDay": "2000-01-01",
+    "city": "Pato Branco",
+    "state": "PR",
+    "neighborhood": "Centro",
+    "street": "Rua Osvaldo Aranha",
+    "houseNumber": 157,
+    "complement": "123",
+    "reference": "teste",
+}
+
+
+def test_create_employers_missing_fields(client):
+    data_copy = data.copy()
+    del data_copy["name"]
+    response = client.post(
+        "employees/create", data=json.dumps(data_copy), headers=headers
+    )
+    print(response, "response")
+    assert response.status_code == 400
+    assert response.json["error"] == "Está faltando o item name"
+
+
+def test_create_employer_invalid_cep_format(client):
+    data_copy = data.copy()
+    data_copy["zipcode"] = "12345678999"
+
+    response = client.post(
+        "/employees/create", data=json.dumps(data_copy), headers=headers
+    )
+
+    assert response.status_code == 400
+    assert response.json["error"] == "Erro em validação - Contate o suporte"
--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,56 +1,39 @@
 import json
-
-from faker import Faker
+from random import randint
 
 mimetype = "application/json"
 url = "/inventory/create"
 
 headers = {"Content-Type": mimetype, "Accept": mimetype}
 
-fake = Faker()
+data = {
+    "codPatrimonio": "teste123" + str(randint(1, 1000)),
+    "title": "Notebook Gamer Lenovo Gaming",
+    "description": "Novo design com 11ª Geração de Processadores Intel Core i5-11300H",
+    "category": "Computador",
+    "value": 3499.99,
+    "brand": "Lenovo",
+    "model": "82MGS00200",
+}
 
 
-def test_create_inventory_sucess(inventory):
-    data = {
-        "_id": fake._id(),
-        "codPatrimonio": fake.codPatrimonio(),
-        "title": fake.title(),
-        "description": fake.description(),
-        "category": fake.category(),
-        "value": fake.value(),
-        "brand": fake.brand(),
-        "model": fake.model(),
-        "collaborator": fake.collaborator(),
-        "createdAt": fake.createdAt(),
-        "updatedAt": fake.updatedAt(),
-    }
+def test_create_inventory_sucess(client):
+    data_copy = data.copy()
 
-    response = inventory.post(
-        "inventory/create", data=json.dumps(data), headers=headers
+    response = client.post(
+        "inventory/create", data=json.dumps(data_copy), headers=headers
     )
 
     assert response.status_code == 201
 
 
-def test_create_inventory_missing_fields(inventory):
-    data = {
-        "_id": fake._id(),
-        "codPatrimonio": fake.codPatrimonio(),
-        "description": fake.description(),
-        "category": fake.category(),
-        "value": fake.value(),
-        "brand": fake.brand(),
-        "model": fake.model(),
-        "collaborator": fake.collaborator(),
-        "createdAt": fake.createdAt(),
-        "updatedAt": fake.updatedAt(),
-    }
+def test_create_inventory_missing_fields(client):
+    data_copy = data.copy()
+    del data_copy["title"]
 
-    response = inventory.post(
-        "inventory/create", data=json.dumps(data), headers=headers
+    response = client.post(
+        "inventory/create", data=json.dumps(data_copy), headers=headers
     )
 
-    assert response.staus_code == 400
-    assert (
-        response.json["error"] == "Está faltando algum campo requerido a ser preenchido"
-    )
+    assert response.status_code == 400
+    assert response.json["error"] == "Está faltando o item title"
