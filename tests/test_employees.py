d65a404cf3ec8333a98c58fadd61f10eab217b77
rafatellescarneiro <rafatellescarneiro@gmail.com>
2022-11-27 23:31:40 -0300

    refact flake8

--- tests/test_employees.py
+++ tests/test_employees.py
@@ -3,13 +3,10 @@ import json
 from random import randint
 from src.app.database.seeds import seeds
 
-
 mimetype = "application/json"
 url = "/employees/create"
 
-
 headers = {"Content-Type": mimetype, "Accept": mimetype}
-
 
 data = {
     "name": "João da Silva",
@@ -28,7 +25,6 @@ data = {
     "reference": "teste",
 }
 
-
 def test_create_employee_success(client, logged_in_client):
     data_copy = data.copy()
     headers["Authorization"] = f"Bearer {logged_in_client}"
@@ -40,7 +36,6 @@ def test_create_employee_success(client, logged_in_client):
     print(response.json, "response")
 
     assert response.status_code == 201
-
 
 def test_create_employee_missing_fields(client, logged_in_client):
     data_copy = data.copy()
@@ -54,7 +49,6 @@ def test_create_employee_missing_fields(client, logged_in_client):
     assert response.status_code == 400
     assert response.json["error"] == "Está faltando o item name"
 
-
 def test_create_employee_invalid_cep_format(client, logged_in_client):
     data_copy = data.copy()
     data_copy["zipcode"] = "1234567899999"
@@ -66,7 +60,6 @@ def test_create_employee_invalid_cep_format(client, logged_in_client):
 
     assert response.status_code == 400
     assert "O campo zipcode não está no formato correto" in response.json["error"]
-
 
 def test_create_employee_invalid_name(client, logged_in_client):
     data_copy = data.copy()
@@ -80,7 +73,6 @@ def test_create_employee_invalid_name(client, logged_in_client):
     assert response.status_code == 400
     assert response.json["error"] == "O campo name não está no formato correto"
 
-
 def test_create_employee_invalid_email(client, logged_in_client):
     data_copy = data.copy()
     data_copy["email"] = "123@abc"
@@ -92,7 +84,6 @@ def test_create_employee_invalid_email(client, logged_in_client):
 
     assert response.status_code == 400
     assert response.json["error"] == "O campo email não está no formato correto"
-
 
 def test_create_employee_invalid_phone(client, logged_in_client):
     data_copy = data.copy()
@@ -106,14 +97,13 @@ def test_create_employee_invalid_phone(client, logged_in_client):
     assert response.status_code == 400
     assert response.json["error"] == "O campo phone não está no formato correto"
 
-
-def test_update_employee_success(client):
+def test_update_employee_success(client, logged_in_client):
     employees = seeds["employees"]
     data_copy = employees[0]
     data_copy["phone"] = "119999-9989"
+    headers["Authorization"] = f"Bearer {logged_in_client}"
+    
     response = client.patch(
         "/employees/update", data=json.dumps(data), headers= headers
     )
-    assert response.status_code == 400
-
-
+    assert response.status_code == 200
